64b8217e321ee00fa35eddb371ee2dffaf0400ab
30263522 <30263522@bsdc.ac.uk>
2024-12-06 11:22:20 +0000

    ersatz

--- ErsatzEnterprises/ersatz.js
+++ ErsatzEnterprises/ersatz.js
@@ -2,10 +2,10 @@ console.log("UwU")
 //###########################
 // Bankdata
 //###########################
-function Bank(name,interest,country,cost) {
+function Bank(name,description,image,cost) {
     this.name = name;
-    this.interest = interest;
-    this.country = country;
+    this.description = description;
+    this.image = image;
     this.cost = cost;
 }
 const bank1 = new Bank("Calpone Vaults","2.5%","America",3500)
@@ -21,33 +21,60 @@ const bank9 = new Bank("ExampleCoin","9%","America",4567)
 banklist=[bank1,bank2,bank3,bank4,bank5,bank6,bank7,bank8,bank9]
 for (let key = 0; key<banklist.length; key++) {
     console.log(banklist[key])
+    const contain = document.getElementById("bank_container");
+
+    if (contain) {
+      console.log(contain)
+      const row = document.createElement('tr');
+
+      const nameCell = document.createElement('td');
+      nameCell.textContent = banklist[key].name
+
+      const imgCell = document.createElement('td');
+      const img = document.createElement('img');
+      img.src = banklist[key].image;
+      img.alt = banklist[key].name;
+      img.width=50
+      imgCell.append(img);
+
+      const descCell = document.createElement('td')
+      descCell.textContent = banklist[key].description
+
+      row.appendChild(nameCell);
+      row.appendChild(imgCell);
+      row.appendChild(descCell);
+
+      contain.appendChild(row);
+    } else {
+      console.error("container not found")
+    }
     }
 
 
 //###########################
 // File Functions
 //###########################
-function download(fileName) {
-    const blob = new Blob([fileName], {type:'text/plain'});
-    const objURL = URL.createObjectURL(blob)
-    const dl = document.createElement('a')
-    dl.id = "dl"
-    dl.href = objURL
-    dl.setAttribute('download', fileName)
-    document.body.appendChild(dl)
-    dl.click()
-    document.getElementById('dl').remove()
-};
-function loadFiles(){
-    let input = document.createElement('input');
-    input.type ='file';
-    input.onchange = _ => {
-        let files = Array.from(input.files);
-        console.log(files);
-    }
+// function download(fileName) {
+//     const blob = new Blob([fileName], {type:'text/plain'});
+//     const objURL = URL.createObjectURL(blob)
+//     const dl = document.createElement('a')
+//     dl.id = "dl"
+//     dl.href = objURL
+//     dl.setAttribute('download', fileName)
+//     document.body.appendChild(dl)
+//     dl.click()
+//     document.getElementById('dl').remove()
+// };
+// function loadFiles(){
+//     let input = document.createElement('input');
+//     input.type ='file';
+//     input.onchange = _ => {
+//         let files = Array.from(input.files);
+//         console.log(files);
+//     }
             
-    input.click()
-}
+//     input.click()
+// }
 
 
 //###########################
