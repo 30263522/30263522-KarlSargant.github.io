e4fbefc7c1acc2b7a59e63b0f42f081eb640bdfc
30263522 <30263522@bsdc.ac.uk>
2025-02-07 09:11:19 +0000

    stage

--- ErsatzEnterprises/ersatz.js
+++ ErsatzEnterprises/ersatz.js
@@ -8,15 +8,15 @@ function Bank(name,description,image,cost) {
     this.description = description;
     this.cost = cost;
 }
-const bank1 = new Bank("Calpone Vaults","2.5%","America",3500)
-const bank2 = new Bank("Emerald Roof", "1%", "South Africa",6540)
-const bank3 = new Bank("Lakes","1%","United Kingdom",5680)
-const bank4 = new Bank("Fences","5%","America",5490)
-const bank5 = new Bank("Bric","2%","Ethiopia",1250)
-const bank6 = new Bank("Rock","1%","Venuzuela",100)
-const bank7 = new Bank("Oiled","4%","UAE",1450)
-const bank8 = new Bank("Bearbyte","3%","America",1987)
-const bank9 = new Bank("ExampleCoin","9%","America",4567)
+const bank1 = new Bank("Calpone Vaults","Calpone Vaults is our oldest partner","FakeIMG.png",3500)
+const bank2 = new Bank("Emerald Roof", "1%", "FakeIMG.png",6540)
+const bank3 = new Bank("Lakes","1%","FakeIMG.png",5680)
+const bank4 = new Bank("Fences","5%","FakeIMG.png",5490)
+const bank5 = new Bank("Bric","2%","FakeIMG.png",1250)
+const bank6 = new Bank("Rock","1%","FakeIMG.png",100)
+const bank7 = new Bank("Oiled","4%","FakeIMG.png",1450)
+const bank8 = new Bank("Bearbyte","3%","FakeIMG.png",1987)
+const bank9 = new Bank("ExampleCoin","9%","FakeIMG.png",4567)
 
 banklist=[bank1,bank2,bank3,bank4,bank5,bank6,bank7,bank8,bank9]
 for (let key = 0; key < banklist.length; key++) {
@@ -41,10 +41,14 @@ for (let key = 0; key < banklist.length; key++) {
       const costCell = document.createElement('td');
       costCell.textContent = banklist[key].cost;
 
+      const subbutton = document.createElement('button')
+      subbutton.textContent = "subscribe"
+
       row.appendChild(nameCell);
       row.appendChild(imgCell);
       row.appendChild(descCell);
       row.appendChild(costCell);
+      row.appendChild(subbutton);
 
       contain.appendChild(row);
   } else {
---
+++ ingtocollegewebsite/app.js
@@ -0,0 +1,6 @@
+function func(){
+    location.replace("https://www.bsdc.ac.uk/")
+}
+function func2(){
+    location.assign("https://www.bsdc.ac.uk/")
+}
